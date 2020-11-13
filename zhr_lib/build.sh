52649f1a4921384bcc4f607e42f2225c41b87840
Alekleon <alekleon@yandex.ru>
2020-11-13 20:15:14 +0300

    cmake poorly runs under Ubuntu..

--- zhr_lib/build.sh
+++ zhr_lib/build.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-#FLEX_PATH must be supplied as parameter from konfig system, or elswere if konfig is not built yet
-FLEX_PATH="../../../../../../3rdparty/flex/flex-2.6.4/distr/bin/Linux/gcc_centos7_x64_centos7_x64-2.0/"
+#initial input values
+CXX_COMP=g++
+C_COMP=gcc
+COMP_MODE=Debug
+BUILD_DIR=build
+BUILD_NAME=zhr
 
-FLEX_DIR_V=$FLEX_PATH
-FLEX_EXECUTABLE_V=${FLEX_DIR_V}/flex
+while [ -n "$1" ]
+do
+	case "$1" in
+		-D) COMPILE_MODE=Debug ;;
+		-R) COMPILE_MODE=Release ;;
+		-d) 
+			shift
+			if [ -n "$1" ]; then
+				BUILD_DIR=$1
+			fi
+			;;			
+		-f) 
+			shift
+			if [ -n "$1" ]; then
+				BUILD_NAME=$1
+			fi
+			;;			
+		*) echo "$1 не опция" ;;
+	esac
+	shift
+done
 
-CXX_C=g++
-C_C=gcc
-
-function help
-{
-echo "info: $1 compilator not supplied as parameter"
-echo "info: $0 <c++ compilator> <c compilator> [intermediate_dir]"
-echo "info: Example : $0 /opt/cross/gcc_centos7_x64_centos7_x64-2.0/bin/x86_64-pc-linux-g++ /opt/cross/gcc_centos7_x64_centos7_x64-2.0/bin/x86_64-pc-linux-gcc"
-echo "info: using default system compilator"
-}
-
-if [[ -z "$2" ]]; then
-C_C=gcc
-help "c++"
-else
-C_C=$2
-fi
-
-if [[ -z "$1" ]]; then
-C_C=gcc
-help "c"
-else
-CXX_C=$1
-fi
-
-rm -rf ./build
-install -d build
-cd build
-cmake -DFLSEX_DIR=${FLEX_DIR_V} -DFLEX_EXECUTABLE=${FLEX_EXECUTABLE_V} -DCMAKE_CXX_COMPILER=$CXX_C -DCMAKE_C_COMPILER=$C_C ../
+rm -rf ./"$BUILD_DIR"
+install -d "$BUILD_DIR"
+cd "$BUILD_DIR"
+cmake -DCMAKE_CXX_COMPILER=$CXX_COMP -DCMAKE_C_COMPILER=$C_COMP -DCMAKE_BUILD_TYPE=$COMP_MODE ../
 make
